a4dff23e2e079d9e67ba21dcec2df5ec80a54acd
Tushar <115475413+TusharSatija@users.noreply.github.com>
2023-09-11 23:37:29 +0530

    bool_matrix.cpp

--- 01-09/bool_matrix.cpp
+++ 01-09/bool_matrix.cpp
@@ -1,30 +1,55 @@
 #include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 int main () {
-	int M,N;
-	cin>>M>>N;
+	int n,m;
+	cin>>n>>m;
 	int arr[1000][1000];
-	//input 2d array
-	for(int i=0;i<M;i++){
-		for(int j=0;j<N;j++){
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<m;j++)
+		{
 			cin>>arr[i][j];
 		}
 	}
-	int row,col;
-	//check which row or col is 1
-	for(int i=0;i<M;i++){
-		for(int j=0;j<N;j++){
-			if(arr[i][j]==1){
-				row=i;
-				col=j;
+	int row[1000]={0};
+	int col[1000]={0};
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<m;j++)
+		{
+			if(arr[i][j]==1)
+			{
+				row[i]=1;
+				col[j]=1;
 			}
 		}
 	}
-	for(int i=0;i<M;i++){
-		for(int j=0;j<N;j++){
-			if(i==row||j==col){
+	for(int j=0;j<m;j++)
+	{
+		if(col[j])
+		{
+			for(int i=0;i<n;i++)
+			{
 				arr[i][j]=1;
 			}
+		}
+	}
+	for(int i=0;i<n;i++)
+	{
+		if(row[i])
+		{
+			for(int j=0;j<m;j++)
+			{
+				arr[i][j]=1;
+			}
+		}
+	}
+
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<m;j++)
+		{
 			cout<<arr[i][j]<<" ";
 		}
 		cout<<endl;
