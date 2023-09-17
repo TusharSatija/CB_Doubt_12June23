04f2f056d3dce7cfb15808255ab2b760ca8d2be6
TusharSatija <tusharsatija737@gmail.com>
2023-09-17 23:06:20 +0530

    recursion

--- 01-09/spiral.cpp
+++ 01-09/spiral.cpp
@@ -2,38 +2,47 @@
 using namespace std;
 
 int main() {
-int n,m;
-cin>>n>>m;
-int arr[100][100];
-for(int i=0;i<n;i++){
-	for(int j=0;j<m;j++){
-		cin>>arr[i][j];
-	}
-}
-int top=0;int bottom=n-1;
-int left=0;int right=m-1;
-while(top<=bottom and left<=right){
-for(int i=left;i<=right;i++){
-	cout<<arr[top][i]<<", ";
-}
-top++;
-for(int j=top;j<=bottom;j++){
-	cout<<arr[j][right]<<", ";
-}
-right--;
-// if(left<=right){
-// 	for(int i=right;i<=left;i--){
-// 		cout<<arr[bottom][i]<<", ";
-// 	}
-// 	bottom--;
-// }
-// if(top<bottom){
-// 	for(int j=bottom;j<=top;j--){
-// 		cout<<arr[j][left]<<", ";
-// 	}
-// 	left++;
-// }
-}
-cout<<"END";
-	return 0;
+    int n, m;
+    cin >> n >> m;
+    int arr[100][100];
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    int top = 0;
+    int bottom = n - 1;
+    int left = 0;
+    int right = m - 1;
+    
+    while (top <= bottom && left <= right) {
+        for (int i = left; i <= right; i++) {
+            cout << arr[top][i]<<", ";
+        }
+        top++;
+        
+        for (int j = top; j <= bottom; j++) {
+            cout << arr[j][right]<< ", ";
+        }
+        right--;
+
+        if (top <= bottom) {
+            for (int i = right; i >= left; i--) {
+                cout << arr[bottom][i]<< ", ";
+            }
+            bottom--;
+        }
+
+        if (left <= right) {
+            for (int j = bottom; j >= top; j--) {
+                cout << arr[j][left] << ", ";
+            }
+            left++;
+        }
+    }
+    
+    cout << "END";
+    return 0;
 }
